a55ae131cb934549dfb4603c1ac3c37e8f278b32
増田隼也 <masudashunya@cquil.local>
2023-08-19 23:04:30 +0900

    df

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ window.addEventListener('DOMContentLoaded', async () => {
           tweetsContainer.appendChild(tweetElement);
       });
   } catch (error) {
-      console.error('Fetch error: ', error);
+      // console.error('Fetch error: ', error);
+
+      const response = await fetch('/fetch-tweets');
+      if (!response.ok) {
+          throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      data.forEach(tweet => {
+          const tweetElement = document.createElement('div');
+          tweetElement.textContent = tweet.text;
+          tweetsContainer.appendChild(tweetElement);
+      });
+      
   }
 });
